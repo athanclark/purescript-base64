6f604bfd5753dca9718f8b9783b3346fb71857b4
Athan Clark <athan.clark@gmail.com>
2017-04-07 23:14:52 -0600

    adding maybe support

--- src/Data/ArrayBuffer/Base64.js
+++ src/Data/ArrayBuffer/Base64.js
@@ -4,4 +4,14 @@ var nacl_util = require('tweetnacl-util');
 
 exports.encodeBase64 = nacl_util.encodeBase64;
 
-exports.decodeBase64 = nacl_util.decodeBase64;
+exports.decodeBase64Impl = function (s) {
+  var r;
+  try {
+    r = nacl_util.decodeBase64(s);
+  } catch(e) {
+    console.warn("Base64 decoding error: ", e, ", data: ", s);
+    r = null;
+  }
+
+  return r;
+};
--- src/Data/ArrayBuffer/Base64.purs
+++ src/Data/ArrayBuffer/Base64.purs
@@ -1,7 +1,17 @@
-module Data.ArrayBuffer.Base64 where
+module Data.ArrayBuffer.Base64
+  ( decodeBase64
+  , encodeBase64
+  ) where
 
 import Data.ArrayBuffer.Types (Uint8Array)
+import Data.Maybe (Maybe)
+import Data.Nullable (Nullable, toMaybe)
+
 
 foreign import encodeBase64 :: Uint8Array -> String
 
-foreign import decodeBase64 :: String -> Uint8Array
+foreign import decodeBase64Impl :: String -> Nullable Uint8Array
+
+
+decodeBase64 :: String -> Maybe Uint8Array
+decodeBase64 s = toMaybe (decodeBase64Impl s)
